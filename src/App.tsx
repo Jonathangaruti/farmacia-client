d2e9709741c91596ad9f85ba055fef4ccaf7c524
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-04-22 13:53:29 -0300

    Crud categoria

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,38 @@ import Home from "./paginas/home/Home";
 import Navbar from "./components/navBar/NavBar";
 import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from './paginas/login/Login';
+import Login from "./paginas/login/Login";
+import Cadastro from "./paginas/cadastro/Cadastro";
+import ListaCategoria from "./components/categorias/listaCategoria/ListaCategoria";
+import FormularioCategoria from "./components/categorias/formularioCategoria/FormularioCategoria";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
-          <div className='min-h-[77.5vh]'>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[77.5vh]">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategoria />} />
+              <Route
+                path="/cadastroCategoria"
+                element={<FormularioCategoria />}
+              />
+              <Route
+                path="/editarCategoria/:id"
+                element={<FormularioCategoria />}
+              />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
   );
 }
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,28 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
- 
-  
-
   return (
     <>
-     <div className='w-full bg-green-300 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Farmacia</div>
+      <div className="w-full bg-green-300 text-white flex justify-center py-4">
+        <div className="container flex justify-between text-lg">
+          <div className="text-2xl font-bold uppercase">Farmacia</div>
 
-            <div className='flex gap-4'>
-            <Link to='/login' className='hover:underline'>Login</Link>
-            <Link to='/home' className='hover:underline'>Home</Link>
-              <div className='hover:underline'>Produtos</div>
-              <div className='hover:underline'>Categoria</div>
-              <div className='hover:underline'>Cadastrar categoria</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
-            </div>
+          <div className="flex gap-4">
+            <Link to="/login" className="hover:underline">
+              Login
+            </Link>
+            <Link to="/home" className="hover:underline">
+              Home
+            </Link>
+            <div className="hover:underline">Produtos</div>
+            <Link to="/Categorias" className="hover:underline">
+              Categorias
+            </Link>
+            <Link to="/cadastroCategoria" className="hover:underline">
+              Cadastrar categoria
+            </Link>
+            <Link to="/perfil" className="hover:underline">
+              Perfil
+            </Link>
           </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
